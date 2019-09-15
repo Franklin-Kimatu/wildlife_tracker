4bca3edc4d3a578e7e01ebdd150ade2ac162a65a
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-15 09:31:51 +0300

    adding the all functionality to the thriving class

--- src/main/java/ThrivingAnimal.java
+++ src/main/java/ThrivingAnimal.java
@@ -1,9 +1,19 @@
 import org.sql2o.*;
+
+import java.util.List;
+
 public class ThrivingAnimal extends Animal{
 
     public ThrivingAnimal(String animalname, int sightingId) {
 
         this.animalname =animalname;
         this.sightingId=sightingId;
+    }
+
+    public static List<ThrivingAnimal> all(){
+        String sql ="SELECT * FROM animals WHERE type ='thriving'";
+        try(Connection con =DB.sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(ThrivingAnimal.class);
+        }
     }
 }
