a1baa1678c0d51d5e197227986f9abe77f732a3b
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-15 10:18:47 +0300

    adding the type attribute

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,8 +1,21 @@
 import org.sql2o.*;
+
+import java.util.List;
+
 public class EndangeredAnimal extends Animal {
+
+    public static final String DATABASE_TYPE ="endangered";
 
     public EndangeredAnimal(String animalname, int sightingId){
         this.animalname=animalname;
         this.sightingId=sightingId;
+        type=DATABASE_TYPE;
+    }
+
+    public static List<EndangeredAnimal> all(){
+        String sql ="SELECT * FROM animals WHERE type ='endangered'";
+        try(Connection con =DB.sql2o.open()){
+            return con.createQuery(sql).executeAndFetch(EndangeredAnimal.class);
+        }
     }
 }
