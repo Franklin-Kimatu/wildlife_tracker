63e52bd5ae00b240482bb1bfe55c5047f885b9e5
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-15 11:08:23 +0300

    endangered animasl can be found by id from the databse

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -12,6 +12,14 @@ public class EndangeredAnimal extends Animal {
         type=DATABASE_TYPE;
     }
 
+    public static EndangeredAnimal find(int id){
+        try(Connection con = DB.sql2o.open()){
+            String sql ="SELECT *FROM animals where id =:id";
+            EndangeredAnimal animal =con.createQuery(sql).addParameter("id",id).executeAndFetchFirst(EndangeredAnimal.class);
+            return animal;
+        }
+    }
+
     public static List<EndangeredAnimal> all(){
         String sql ="SELECT * FROM animals WHERE type ='endangered'";
         try(Connection con =DB.sql2o.open()){
