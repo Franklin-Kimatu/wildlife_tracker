82fc0ac7381012e6a856491cb4992452fd16a6d8
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-15 15:30:51 +0300

    addign the index pges

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.ArrayList;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/",(request, response) -> {
+            Map<String,Object> model =new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
